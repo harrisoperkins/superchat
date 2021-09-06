c4a90e949a8fca92dbdfa48b65793e1f607c2d5a
harrisoperkins <harrisonperkins@hotmail.com>
2021-09-06 19:14:46 +0100

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //config
+  apiKey: "AIzaSyDTK5lwVd7PZ-7rLY5iod1Ddtl5DA0wPVY",
+  authDomain: "superchat-18bec.firebaseapp.com",
+  projectId: "superchat-18bec",
+  storageBucket: "superchat-18bec.appspot.com",
+  messagingSenderId: "239438557866",
+  appId: "1:239438557866:web:d662a4a3650f15cb9656d3",
+  measurementId: "G-JNGBFS96L5"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth); 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+
 }
 
 export default App;
